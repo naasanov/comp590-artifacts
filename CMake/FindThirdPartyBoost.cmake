b50a640659b0027b94c3d0bcd0219f8c9982c525
Thomas Prampart <thomas.prampart@inria.fr>
2023-06-27 11:37:25 +0200

    create conda env

--- CMake/FindThirdPartyBoost.cmake
+++ CMake/FindThirdPartyBoost.cmake
@@ -9,7 +9,9 @@ if(EXISTS ${LIST_DEPENDENCIES_PATH}/boost)
     set(ENV{BOOST_ROOT} ${LIST_DEPENDENCIES_PATH}/boost)
 endif()
 
-set(Boost_USE_STATIC_LIBS ON)
+#set(Boost_NO_SYSTEM_PATHS ON)
+#set(Boost_USE_MULTITHREAD ON)
+set(Boost_USE_STATIC_LIBS OFF)
 find_package(Boost 1.71.0 COMPONENTS chrono filesystem regex system thread)
 
 set_property(GLOBAL PROPERTY OV_TRIED_ThirdPartyBoost "Yes")
