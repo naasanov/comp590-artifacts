11b1c0b3535741b0286215949c90cdd492ade045
Thomas <thomas.prampart@gmail.com>
2023-09-29 09:29:51 +0200

    Revert multi inclusion checks on cmake modules that are meant to be called only once

--- CMake/FindThirdPartyGTK2.cmake
+++ CMake/FindThirdPartyGTK2.cmake
@@ -17,20 +17,18 @@
 # If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
-if(NOT GTK2_FOUND)
-    if(EXISTS ${LIST_DEPENDENCIES_PATH}/gtk)
-        set(GTK2_GDKCONFIG_INCLUDE_DIR ${LIST_DEPENDENCIES_PATH}/gtk/lib/gtk-2.0/include)
-        set(GTK2_GLIBCONFIG_INCLUDE_DIR ${LIST_DEPENDENCIES_PATH}/gtk/lib/glib-2.0/include)
-    endif()
+if(EXISTS ${LIST_DEPENDENCIES_PATH}/gtk)
+    set(GTK2_GDKCONFIG_INCLUDE_DIR ${LIST_DEPENDENCIES_PATH}/gtk/lib/gtk-2.0/include)
+    set(GTK2_GLIBCONFIG_INCLUDE_DIR ${LIST_DEPENDENCIES_PATH}/gtk/lib/glib-2.0/include)
+endif()
 
-    find_package(PkgConfig REQUIRED)
-    pkg_check_modules(GTK2 REQUIRED gtk+-2.0)
+find_package(PkgConfig REQUIRED)
+pkg_check_modules(GTK2 REQUIRED gtk+-2.0)
 
-    if(GTK2_FOUND)
-        ov_print(OV_PRINTED "Found GTK2 library")
+if(GTK2_FOUND)
+    ov_print(OV_PRINTED "Found GTK2 library")
 
-        add_library(gtk2 INTERFACE)
-        target_include_directories(gtk2 INTERFACE ${GTK2_INCLUDE_DIRS})
-        target_link_libraries(gtk2 INTERFACE ${GTK2_LIBRARIES})
-    endif()
+    add_library(gtk2 INTERFACE)
+    target_include_directories(gtk2 INTERFACE ${GTK2_INCLUDE_DIRS})
+    target_link_libraries(gtk2 INTERFACE ${GTK2_LIBRARIES})
 endif()
--- CMake/FindThirdPartyOpenAL.cmake
+++ CMake/FindThirdPartyOpenAL.cmake
@@ -37,49 +37,44 @@ endif()
 ##
 # Find ALUT
 #
-if (NOT ALUT_FOUND)
-    pkg_check_modules(ALUT freealut)
+pkg_check_modules(ALUT freealut)
 
-    if (ALUT_FOUND)
-        ov_print(OV_PRINTED "Found ALUT")
-        add_library(alut INTERFACE)
+if (ALUT_FOUND)
+    ov_print(OV_PRINTED "Found ALUT")
+    add_library(alut INTERFACE)
 
-        set(ALUT_LINK_LIB "ALUT_LINK_LIB-NOTFOUND")
-        find_library(ALUT_LINK_LIB NAMES alut PATHS ${ALUT_LIBRARY_DIRS} NO_DEFAULT_PATH)
-        if (ALUT_LINK_LIB)
-            target_include_directories(alut INTERFACE ${ALUT_INCLUDE_DIRS})
-            target_link_libraries(alut INTERFACE ${ALUT_LINK_LIB} OpenAL::OpenAL)
-            target_compile_definitions(alut INTERFACE -DTARGET_HAS_ThirdPartyOpenAL)
-        else()
-            ov_print(OV_PRINTED "Found ALUT, but failed to find the lib")
-        endif()
-    else ()
-        ov_print(OV_PRINTED "Failed to find ALUT")
+    set(ALUT_LINK_LIB "ALUT_LINK_LIB-NOTFOUND")
+    find_library(ALUT_LINK_LIB NAMES alut PATHS ${ALUT_LIBRARY_DIRS} NO_DEFAULT_PATH)
+    if (ALUT_LINK_LIB)
+        target_include_directories(alut INTERFACE ${ALUT_INCLUDE_DIRS})
+        target_link_libraries(alut INTERFACE ${ALUT_LINK_LIB} OpenAL::OpenAL)
+        target_compile_definitions(alut INTERFACE -DTARGET_HAS_ThirdPartyOpenAL)
+    else()
+        ov_print(OV_PRINTED "Found ALUT, but failed to find the lib")
     endif()
+else ()
+    ov_print(OV_PRINTED "Failed to find ALUT")
 endif()
 
 
 ##
 # Find OGG
 #
+pkg_check_modules(OGG ogg)
+if (OGG_FOUND)
+    ov_print(OV_PRINTED "Found OGG")
+    add_library(ogg INTERFACE)
 
-if (NOT OGG_FOUND)
-    pkg_check_modules(OGG ogg)
-    if (OGG_FOUND)
-        ov_print(OV_PRINTED "Found OGG ++++")
-        add_library(ogg INTERFACE)
-
-        set(OGG_LINK_LIB "OGG_LINK_LIB-NOTFOUND")
-        find_library(OGG_LINK_LIB NAMES ogg PATHS ${OGG_LIBRARY_DIRS} NO_DEFAULT_PATH)
-        if(OGG_LINK_LIB)
-            target_include_directories(ogg INTERFACE ${OGG_INCLUDE_DIRS})
-            target_link_libraries(ogg INTERFACE ${OGG_LINK_LIB})
-        else()
-            ov_print(OV_PRINTED "Found OGG, but failed to find the lib")
-        endif()
+    set(OGG_LINK_LIB "OGG_LINK_LIB-NOTFOUND")
+    find_library(OGG_LINK_LIB NAMES ogg PATHS ${OGG_LIBRARY_DIRS} NO_DEFAULT_PATH)
+    if(OGG_LINK_LIB)
+        target_include_directories(ogg INTERFACE ${OGG_INCLUDE_DIRS})
+        target_link_libraries(ogg INTERFACE ${OGG_LINK_LIB})
     else()
-        ov_print(OV_PRINTED "Failed to find OGG")
+        ov_print(OV_PRINTED "Found OGG, but failed to find the lib")
     endif()
+else()
+    ov_print(OV_PRINTED "Failed to find OGG")
 endif()
 
 ##
@@ -87,20 +82,18 @@ endif()
 #
 # Linux conda package for OGG not providing OggConfig.cmake and Vorbis needs it so find_package(Vorbis) cannot be used either.
 
-if (VORBIS_LIB-NOTFOUND)
-    find_library(VORBIS_LIB NAMES vorbisfile)
-    find_path(VORBIS_INCLUDE_DIR NAMES vorbisfile.h PATH_SUFFIXES vorbis)
+find_library(VORBIS_LIB NAMES vorbisfile)
+find_path(VORBIS_INCLUDE_DIR NAMES vorbisfile.h PATH_SUFFIXES vorbis)
 
-    if(NOT VORBIS_LIB STREQUAL VORBIS_LIB-NOTFOUND
-            AND NOT VORBIS_INCLUDE_DIR STREQUAL VORBIS_INCLUDE_DIR-NOTFOUND)
+if(NOT VORBIS_LIB STREQUAL VORBIS_LIB-NOTFOUND
+        AND NOT VORBIS_INCLUDE_DIR STREQUAL VORBIS_INCLUDE_DIR-NOTFOUND)
 
-            ov_print(OV_PRINTED "Found Vorbis library")
+        ov_print(OV_PRINTED "Found Vorbis library")
 
-	    # Create target to link against.
-	    add_library(vorbis INTERFACE)
-	    target_include_directories(vorbis INTERFACE ${VORBIS_INCLUDE_DIR})
-	    target_link_libraries(vorbis INTERFACE ${VORBIS_LIB} ogg)
-    else()
-	    ov_print(OV_PRINTED "  FAILED to find Vorbis")
-    endif()
+    # Create target to link against.
+    add_library(vorbis INTERFACE)
+    target_include_directories(vorbis INTERFACE ${VORBIS_INCLUDE_DIR})
+    target_link_libraries(vorbis INTERFACE ${VORBIS_LIB} ogg)
+else()
+    ov_print(OV_PRINTED "  FAILED to find Vorbis")
 endif()
