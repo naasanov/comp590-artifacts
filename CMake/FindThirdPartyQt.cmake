fae59c396d85bfbd2fab93ca38caeb7226acf574
CABEL Tristan <tristan.cabel@inria.fr>
2024-07-04 20:52:51 +0200

    Signal display

--- CMake/FindThirdPartyQt.cmake
+++ CMake/FindThirdPartyQt.cmake
@@ -1,12 +1,13 @@
 get_property(OV_PRINTED GLOBAL PROPERTY OV_TRIED_ThirdPartyQt)
 
-find_package(Qt6 REQUIRED COMPONENTS Core Gui Qml Quick QuickControls2 Xml)
+find_package(Qt6 REQUIRED COMPONENTS Charts Core Gui Qml Quick QuickControls2 Widgets Xml)
 
 IF(Qt6_FOUND)
 
     if(QT_KNOWN_POLICY_QTP0001)
         qt_policy(SET QTP0001 NEW)
     endif()
+    set(QT_QML_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/qml)
 
     ov_print(OV_PRINTED "Found Qt library")
 ELSE()
