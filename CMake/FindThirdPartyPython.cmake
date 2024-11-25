349cfdda6ea0e5e7b219aeb4cd3f599e363fcb79
Thomas PRAMPART <thomas.prampart@inria.fr>
2024-11-25 09:17:01 +0100

    Embed python interpreter

--- CMake/FindThirdPartyPython.cmake
+++ CMake/FindThirdPartyPython.cmake
@@ -4,7 +4,7 @@
 
 get_property(OV_PRINTED GLOBAL PROPERTY OV_TRIED_ThirdPartyPython)
 
-
+set(Python3_FIND_VIRTUALENV FIRST)
 if (WIN32)
   # needed when we run cmake in a conda environment
   set(Python3_FIND_REGISTRY LAST)
@@ -14,7 +14,7 @@ if (APPLE)
   set(Python3_FIND_FRAMEWORK LAST)
 endif()
 
-find_package(Python3 3.10 EXACT COMPONENTS Interpreter Development)
+find_package(Python3 3.10 COMPONENTS Interpreter Development)
 
 if(Python3_FOUND)
   OV_PRINT(OV_PRINTED "  Found Python 3 at ${Python3_LIBRARIES}")
@@ -23,9 +23,7 @@ else()
    OV_PRINT(OV_PRINTED "  FAILED to find Python 3 (needs v3.10 with bitness matching build target ${PLATFORM_TARGET})")
 endif()
 
-set(PYTHON_EXECUTABLE ${Python3_EXECUTABLE})
-
-include_directories(${Python3_INCLUDE_DIRS})
-
+get_filename_component(PYTHON3_HOME ${Python3_EXECUTABLE} DIRECTORY)
+target_compile_definitions(Python3::Python INTERFACE -DPYTHON_HOME="${PYTHON3_HOME}")
 
 set_property(GLOBAL PROPERTY OV_TRIED_ThirdPartyPython "Yes")
